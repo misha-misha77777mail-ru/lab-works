2944202e640da57e9d423c8a8c56fe0f624d38b0
MMVlasko <vlaskomm@mai.education>
2023-11-12 21:27:29 +0200

    lab-work 10

--- LR9/lab9.c
+++ LR9/lab9.c
@@ -14,11 +14,11 @@ void move(int count) {
     for (int k = 0; k < count; k++) {
         int temp_i = i, temp_j = j, temp_l = l;
 
-        i = MOD(temp_i * MAX(temp_j, temp_l), 20) + MOD(temp_j * MIN(temp_i, temp_l), 30) + k;
+        i = MOD(i * MAX(j, l), 20) + MOD(j * MIN(i, l), 30) + k;
         j = MOD(ABS(temp_i - temp_j + temp_l - k) * SIGN(k - 10), 20);
         l = MOD(ABS(temp_i - temp_j) * temp_l - ABS(temp_j - temp_l) * temp_i + ABS(temp_i - temp_l) * temp_j, 20) - k;
 
-        if ((((double)((i - 20) * (i - 20)) / 100.0) + ((double)(j * j) / 25.0)) <= 1 && result_k == -1 && k <= 50) {
+        if ((((float)((i - 20) * (i - 20)) / 100.0) + ((float)(j * j) / 25.0)) <= 1 && result_k == -1 && k <= 50) {
             result_k = k;
         }
     }

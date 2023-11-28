31124cbb2841795481b708163bc774e4fe6fd77b
MMVlasko <vlaskomm@mai.education>
2023-11-28 08:26:27 +0200

    completed

--- LR13/lab13.c
+++ LR13/lab13.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-
-typedef enum
-{
-    FIND,
-    READ,
-    SKIP
-} State;
+#include <stddef.h>
+#include <wctype.h>
 
 int separator(char s) {
     char *m = " \t,\n";
@@ -15,99 +10,60 @@ int separator(char s) {
     return 0;
 }
 
-int sonorous(char s) {
-    char *m = "бвгджзлмнрйБВГДЖЗЛМНРЙ";
-    for (int i = 0; i < 12; i++) {
-        if (s == m[i]) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int deaf(char s) {
-    char *m = "пфктшсчщхцПФКТШСЧЩХЦ";
-    for (int i = 0; i < 12; i++) {
-        if (s == m[i]) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
 int main(void) {
+    const unsigned int lu = 1;
+    const wchar_t a = L'б';
+
+    const unsigned int sonorous = (
+        lu << (L'б' - a) |
+        lu << (L'в' - a) |
+        lu << (L'г' - a) |
+        lu << (L'д' - a) |
+        lu << (L'ж' - a) |
+        lu << (L'з' - a) |
+        lu << (L'л' - a) |
+        lu << (L'м' - a) |
+        lu << (L'н' - a) |
+        lu << (L'р' - a) |
+        lu << (L'й' - a)
+    );
+
+    const unsigned int deaf = (
+        lu << (L'п' - a) |
+        lu << (L'ф' - a) |
+        lu << (L'к' - a) |
+        lu << (L'т' - a) |
+        lu << (L'ш' - a) |
+        lu << (L'с' - a) |
+        lu << (L'ч' - a) |
+        lu << (L'щ' - a) |
+        lu << (L'х' - a) |
+        lu << (L'ц' - a)
+    );
+
+    unsigned int word = 0;
     int counter = 0;
-
-    int chr;
-    int flag = 0;
-    int deaf_flag = 0;
-
-    State state = FIND;
+    wchar_t chr;
 
     while ((chr = getchar()) != EOF) {
-        switch (state) {
-            case SKIP:
-            {
-                if (separator(chr)) state = FIND;
+        if (separator(chr) && word) {
+            if (((word & sonorous) <= 1) && (word & deaf)) {
+                counter++;
             }
-            break;
-
-            case FIND:
-            {
-                if (!separator(chr)) {
-                    if (deaf(chr) && !deaf_flag) {
-                        deaf_flag = 1;
-                    } else if (deaf(chr) && deaf_flag == 1) {
-                        deaf_flag = 2;
-                    }
-
-                    if (sonorous(chr) && !flag) {
-                        flag = 1;
-                    } else if (sonorous(chr) && flag) {
-                        state = SKIP;
-                        flag = 0;
-                        deaf_flag = 0;
-                    } else {
-                        state = READ;
-                        flag = 0;
-                        if (deaf_flag != 2) deaf_flag = 0;
-                    }
-                }
-            }
-            break;
-
-            case READ:
-            {
-                if (deaf(chr) && !deaf_flag) {
-                        deaf_flag = 1;
-                } else if (deaf(chr) && deaf_flag == 1) {
-                        deaf_flag = 2;
-                }
-
-                if (separator(chr) && deaf_flag == 2) {
-                    counter++;
-                    state = FIND;
-                    deaf_flag = 0;
-                } else if (sonorous(chr) && !flag) {
-                    flag = 1;
-                } else if (sonorous(chr) && flag) {
-                    flag = 0;
-                    state = SKIP;
-                    deaf_flag = 0;
-                } else {
-                    flag = 0;
-                    if (deaf_flag != 2) deaf_flag = 0;
-                }
-            }
+            word = 0;
+        } else {
+            word = word | lu << (towlower(chr) - a);
         }
     }
 
-    if (state == READ && deaf_flag == 2) counter++;
+    if (((word & sonorous) <= 1) && (word & deaf)) counter++;
 
     if (counter) {
-        printf("\nThere are words in which all consonants are deaf. The number of such words: %d.", counter);
+        printf("\nThere are words in which all consonants are deaf.\n");
+        printf("The number of such words: %d.", counter);
     } else {
         printf("\nThere are no words in which all consonants are deaf.");
     }
+
     return 0;
 }
